f86b50c1cdb634dbdd9d4ecd9cc3c826cfdfa044
sofiaroz29 <101563492+sofiaroz29@users.noreply.github.com>
2022-05-20 11:37:30 -0300

    bases, plataforma y rebote

---
+++ Assets/PlatformBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBehaviour : MonoBehaviour
+{
+
+    public float speed;
+    public bool toRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toRight == true)
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+
+        else
+        {
+            transform.position -= new Vector3(speed, 0, 0);
+        }
+
+        if (transform.position.x > 4)
+        {
+            toRight = false;
+        }
+        
+        if (transform.position.x < -4f )
+        {
+            toRight = true;
+        }
+    }
+}
